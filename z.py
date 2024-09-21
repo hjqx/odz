e5f74fbc34bf7549cf611d0a89946068f06f6697
hjqx <180844972+hjqx@users.noreply.github.com>
2024-09-21 18:31:03 +0200

    return a dict instead + confuse indexing

--- z.py
+++ z.py
@@ -1,13 +1,23 @@
-# func targzet
+# func target
 def x (obj):
-    a=2
+    a,c=2,3
     b="hello,world!"
     return obj.__call__()
 
-# retourne une list[tuple] des locals const et var d'une fonc
+# retourne un dictionnaire des locals const et var d'une fonc
 def mix (varnames : object, consts : object):
-    return list(zip(varnames.__code__.co_varnames,
+    return dict(zip(varnames.__code__.co_varnames,
         consts.__code__.co_consts))
 
-for tt in (lambda e : mix(e,e))(x) : #peut etre très utile pour mes obfs
-    print(tt)
+d = (lambda e : mix(e,e))(x)
+
+print(d["c"]) 
+            # b var ignored cause he does not have const 
+            # (('obj', 'a', 'c', 'b'), (None, (2, 3), 'hello,world!'))
+            # so a : (2,3)  (counting as a tuple)
+            #    c : 'hello,world' 
+            #    indexing c and having "hello world" could be pretty confusing imo xD
+
+
+# for tt in (lambda e : mix(e,e))(x).items() : 
+#     print(tt)
